ddfa7cfc32b325d2e712ca372abe4918d802b28b
jjhlzn <jinjunhang@hotmail.com>
2016-05-04 14:28:08 +0800

    update

--- app/controllers/servicelocator_controller.rb
+++ app/controllers/servicelocator_controller.rb
@@ -2,10 +2,16 @@ class ServicelocatorController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:index]
   def index
     app = params[:app]
+    version = params[:version]
   
     if app == "huayuan_contract"
-      render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'oa.lloydind.com', port: 10080}} 
-      return
+      if version == "1.0.2-2" 
+        render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jjhtest.hengdianworld.com', port: 80}} 
+        return
+      else
+        render json: {status: 0, errorMessage: '', result: {http: 'https', serverName: 'oa.lloydind.com', port: 10443}} 
+        return
+      end
     elsif app == "huayuan_contract_android"
       render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jjhtest.hengdianworld.com', port: 80}} 
       return
