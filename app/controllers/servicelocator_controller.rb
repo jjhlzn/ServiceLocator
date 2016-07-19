97ebec27f637e084d5c73d1b72760be33bfdde84
jjhlzn <jinjunhang@hotmail.com>
2016-07-19 08:52:51 +0800

    update

--- app/controllers/servicelocator_controller.rb
+++ app/controllers/servicelocator_controller.rb
@@ -1,28 +1,11 @@
 class ServicelocatorController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:index]
   def index
-    app = params[:app]
-    version = params[:version]
-  
-    if app == "huayuan_contract"
-      if version == "1.0.2-2" 
-        render json: {status: 0, errorMessage: '', result: {http: 'https', serverName: 'oa.lloydind.com', port: 10443}}  
-        return
-      else
-        render json: {status: 0, errorMessage: '', result: {http: 'https', serverName: 'oa.lloydind.com', port: 10443}} 
-        return
-      end
-    elsif app == "huayuan_contract_android"
-      if version == "1.0"
-        render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jjhtest.hengdianworld.com', port: 80}} 
-      elsif version == "1.0.2"
-        render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jjhtest.hengdianworld.com', port: 80}}
-      else
-        render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jjhtest.hengdianworld.com', port: 80}}
-      end 
-      return
-    end
-  
-    render json: {status: 0, errorMessage: '', result: {}} 
+    
+    platform = params[:client][:platform]
+    Rails.logger.debug {platform}
+
+    render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: '114.215.236.171', port: 6012}}
+    
   end
 end
