22a30da3785d89a03007bb41548a1657a0757046
jjhlzn <jinjunhang@hotmail.com>
2016-08-24 14:39:32 +0800

    update service locator for iPhone

--- app/controllers/servicelocator_controller.rb
+++ app/controllers/servicelocator_controller.rb
@@ -3,7 +3,13 @@ class ServicelocatorController < ApplicationController
   def index
     
     platform = params[:client][:platform]
+    version = params[:client][:appversion]
     Rails.logger.debug {platform}
+
+    if platform == 'iphone' && version == '1.0.1.45'
+      render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jf.jinjunhang.com', port: 80}}
+      return
+    end
 
     render json: {status: 0, errorMessage: '', result: {http: 'http', serverName: 'jf.yhkamani.com', port: 80}}
     
